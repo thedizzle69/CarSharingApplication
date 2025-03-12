eefcdc9c74ab851604032c7ba49e084e9da83e54
thedizzle69 <ic22b037@technikum-wien.at>
2025-03-13 00:11:31 +0100

    Added javadoc comments

--- src/main/java/org/example/carsharingapplication/CarSharingApplication.java
+++ src/main/java/org/example/carsharingapplication/CarSharingApplication.java
@@ -1,5 +1,6 @@
 package org.example.carsharingapplication;
 
+//Getter and Setter Annotations:
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.SpringApplication;
@@ -18,13 +19,53 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 
+/**
+ * This class is the main entry point for the Car Sharing Application.
+ *
+ * @version 1.0
+ * @since 13-03-2025
+ * @author thedizzle69
+ *
+ */
+
+
+
+/** This class is the User class to be used by the Car Sharing Application.
+ *
+ * @version 1.0
+ * @since 13-03-2025
+ * @author thedizzle69
+ *
+ * @param username The username of the user
+ * @param password The password of the user
+ * @param role The role of the user
+ *
+ */
+
 class User {
-    // Getters and setters
+
     private String username;
     private String password;
-    private String role; // Assuming role is either "fleet-manager" or customer
+    private String role; // Possible roles: "fleet-manager", "driver"
 
 }
+
+/** This class is the Vehicle class to be used by the Car Sharing Application.
+ *
+ * @version 1.0
+ * @since 13-03-2025
+ * @author thedizzle69
+ *
+ * @param id The id of the vehicle
+ * @param name The name of the vehicle
+ * @param description The description of the vehicle
+ * @param latitude The latitude of the vehicle
+ * @param longitude The longitude of the vehicle
+ * @param state The state of the vehicle
+ * @param currentDriver The current driver of the vehicle
+ * @param vehicleToken The token of the vehicle
+ *
+ */
 
 class Vehicle {
     private long id;
