feddf5d0cde81a10199d6a26561862c5f82bfd76
thedizzle69 <ic22b037@technikum-wien.at>
2025-03-13 00:01:12 +0100

    Created CarSharingApplicationTest.java. Implemented User Class with successful test.

---
+++ src/main/java/org/example/carsharingapplication/CarSharingApplication.java
@@ -0,0 +1,46 @@
+package org.example.carsharingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api")
+
+class User {
+    private String username;
+    private String password;
+    private String role; // Assuming role is either "fleet-manager" or customer
+
+    // Getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
