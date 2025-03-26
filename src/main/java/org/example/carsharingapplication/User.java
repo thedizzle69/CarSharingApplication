b8ad69fa74ec0ece18fc28ff7eb86e898e0fdf8a
thedizzle69 <ic22b037@technikum-wien.at>
2025-03-26 07:21:02 +0100

    Tried lombok again. Had to set "Obtain classpath" thing in compiler -> annotation setting

--- src/main/java/org/example/carsharingapplication/User.java
+++ src/main/java/org/example/carsharingapplication/User.java
@@ -1,6 +1,8 @@
 package org.example.carsharingapplication;
 
 
+import lombok.*;
+
 /**
  * This class represents a User in the Car Sharing Application.
  *
@@ -8,6 +10,12 @@ package org.example.carsharingapplication;
  * @since 13-03-2025
  * @author Jai SINGH
  */
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class User {
     private String username;
@@ -18,91 +26,5 @@ public class User {
     private Integer age;
     private String licenseNo;
     private String creditCardNo;
-
-    public User(String username, String password, String role, String firstName, String lastName, Integer age, String licenseNo, String creditCardNo) {
-        this.username = username;
-        this.password = password;
-        this.role = role;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.licenseNo = licenseNo;
-        this.creditCardNo = creditCardNo;
-    }
-
-    //Constructor
-
-    public User() {
-    }
-
-    // Getters and Setters
-
-    //Getters
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public String getLicenseNo() {
-        return licenseNo;
-    }
-
-    public String getCreditCardNo() {
-        return creditCardNo;
-    }
-
-    //Setters
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public void setLicenseNo(String licenseNo) {
-        this.licenseNo = licenseNo;
-    }
-
-    public void setCreditCardNo(String creditCardNo) {
-        this.creditCardNo = creditCardNo;
-    }
 
 }
--- src/main/java/org/example/carsharingapplication/Vehicle.java
+++ src/main/java/org/example/carsharingapplication/Vehicle.java
@@ -1,5 +1,7 @@
 package org.example.carsharingapplication;
 
+import lombok.*;
+
 /**
  * This class represents a Vehicle in the Car Sharing Application.
  *
@@ -7,6 +9,12 @@ package org.example.carsharingapplication;
  * @since 13-03-2025
  * @author Jai SINGH
  */
+
+@Getter
+@Setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Vehicle {
     private long id;
@@ -17,91 +25,5 @@ public class Vehicle {
     private String state;
     private String currentDriver;
     private String vehicleToken;
-
-    public Vehicle(long id, String name, String description, double latitude, double longitude, String state, String currentDriver, String vehicleToken) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.state = state;
-        this.currentDriver = currentDriver;
-        this.vehicleToken = vehicleToken;
-    }
-
-    //Constructor
-
-    public Vehicle() {
-    }
-
-    // Getters and Setters
-
-    //Getters
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public String getCurrentDriver() {
-        return currentDriver;
-    }
-
-    public String getVehicleToken() {
-        return vehicleToken;
-    }
-
-    //Setters
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
-    }
-
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public void setCurrentDriver(String currentDriver) {
-        this.currentDriver = currentDriver;
-    }
-
-    public void setVehicleToken(String vehicleToken) {
-        this.vehicleToken = vehicleToken;
-    }
 
 }
